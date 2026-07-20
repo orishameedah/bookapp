c87bf5e327410ccf46c87a125e922975aa0a5fec
Orimadegun Hameedah Oyindamola <87584594+orishameedah@users.noreply.github.com>
2026-07-20 03:29:28 +0100

    Load API base URL from environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [react()],
     server: {
       proxy: {
-        "/api": "https://crud-seven-wine.vercel.app/",
+        "/api": env.VITE_API_BASE,
       },
     },
   });
