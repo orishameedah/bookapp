a9331716ecf6b35990e749b57e4c10c140a252ac
Orimadegun Hameedah Oyindamola <87584594+orishameedah@users.noreply.github.com>
2026-07-20 03:36:08 +0100

    Comment out env loading and server proxy in config
    Comment out environment loading and server proxy configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  // const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [react()],
-    server: {
-      proxy: {
-        "/api": env.VITE_API_BASE,
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     "/api": env.VITE_API_BASE,
+    //   },
+    // },
   });
 };
